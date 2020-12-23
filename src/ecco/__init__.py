d6fd1b5848eb74c664ae7c8403de3c850ac700f7
nostalgebraist <nostalgebraist@gmail.com>
2020-12-23 13:55:27 -0800

    expose hidden_states and activations_layer_nums in from_pretrained construction route

--- src/ecco/__init__.py
+++ src/ecco/__init__.py
@@ -2,7 +2,12 @@ __version__ = '0.0.10'
 from ecco.lm import LM, MockGPT, MockGPTTokenizer
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-def from_pretrained(hf_model_id, activations=False, attention=False, hidden_states=True):
+def from_pretrained(hf_model_id,
+                    activations=False,
+                    attention=False,
+                    hidden_states=True,
+                    activations_layer_nums=None,
+                    ):
     if hf_model_id == "mockGPT":
         tokenizer = MockGPTTokenizer()
         model = MockGPT()
@@ -11,9 +16,9 @@ def from_pretrained(hf_model_id, activations=False, attention=False, hidden_stat
         model = AutoModelForCausalLM.from_pretrained(hf_model_id,
                                                      output_hidden_states=hidden_states,
                                                      output_attentions=attention)
-    if activations:
-        lm = LM(model, tokenizer, collect_activations_flag=True)
-        return lm
-    else:
-        lm = LM(model, tokenizer)
-        return lm
+
+    lm_kwargs = {
+        'collect_activations_flag': activations,
+        'collect_activations_layer_nums': activations_layer_nums}
+    lm = LM(model, tokenizer, **lm_kwargs)
+    return lm
