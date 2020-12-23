d121a69795fa1f3c8cb58db59ca3965be538a855
nostalgebraist <nostalgebraist@gmail.com>
2020-12-23 14:50:31 -0800

    undo changes that did not work as i expected

--- src/ecco/attribution.py
+++ src/ecco/attribution.py
@@ -4,20 +4,21 @@ import numpy as np
 
 def saliency(prediction_logit, token_ids_tensor_one_hot, norm=True):
     # Back-propegate the gradient from the selected output-logit
-    grad = torch.autograd.grad(prediction_logit, token_ids_tensor_one_hot)[0]
+    prediction_logit.backward(retain_graph=True)
 
     # token_ids_tensor_one_hot.grad is the gradient propegated to ever embedding dimension of
     # the input tokens.
     if norm:  # norm calculates a scalar value (L2 Norm)
-        token_importance_raw = torch.norm(grad, dim=1)
+        token_importance_raw = torch.norm(token_ids_tensor_one_hot.grad, dim=1)
         # print('token_importance_raw', token_ids_tensor_one_hot.grad.shape,
         # np.count_nonzero(token_ids_tensor_one_hot.detach().numpy(), axis=1))
 
         # Normalize the values so they add up to 1
         token_importance = token_importance_raw / torch.sum(token_importance_raw)
     else:
-        token_importance = torch.sum(grad, dim=1)  # Only one value, all others are zero
+        token_importance = torch.sum(token_ids_tensor_one_hot.grad, dim=1)  # Only one value, all others are zero
 
+    token_ids_tensor_one_hot.grad.data.zero_()
     return token_importance
 
 
@@ -25,6 +26,7 @@ def saliency_on_d_embeddings(prediction_logit, inputs_embeds, aggregation="L2"):
     inputs_embeds.retain_grad()
 
     # Back-propegate the gradient from the selected output-logit
+    prediction_logit.backward(retain_graph=True)
 
     # inputs_embeds.grad
     # token_ids_tensor_one_hot.grad is the gradient propegated to ever embedding dimension of
@@ -43,13 +45,18 @@ def saliency_on_d_embeddings(prediction_logit, inputs_embeds, aggregation="L2"):
         token_importance_raw = torch.mean(inputs_embeds.grad, dim=1)
         token_importance = token_importance_raw  # Hmmm, how to normalize if it includes negative values
 
+    inputs_embeds.grad.data.zero_()
     return token_importance
 
 
 def gradient_x_inputs_attribution(prediction_logit, inputs_embeds):
+
     inputs_embeds.retain_grad()
     # back-prop gradient
-    grad = torch.autograd.grad(prediction_logit, inputs_embeds)[0]
+    prediction_logit.backward(retain_graph=True)
+    grad = inputs_embeds.grad
+    # This should be equivalent to
+    # grad = torch.autograd.grad(prediction_logit, inputs_embeds)[0]
 
     # Grad X Input
     grad_x_input = grad * inputs_embeds
@@ -60,4 +67,7 @@ def gradient_x_inputs_attribution(prediction_logit, inputs_embeds):
     # Normalize so we can show scores as percentages
     token_importance_normalized = feature_importance / torch.sum(feature_importance)
 
+    # Zero the gradient for the tensor so next backward() calls don't have
+    # gradients accumulating
+    inputs_embeds.grad.data.zero_()
     return token_importance_normalized
