63877c8e570e6865f213dcf5b7a4425a5ff92b56
nostalgebraist <nostalgebraist@gmail.com>
2020-12-23 14:33:09 -0800

    reduce memory leakage (?) in saliency by avoiding retain_graph

--- src/ecco/attribution.py
+++ src/ecco/attribution.py
@@ -4,19 +4,19 @@ import numpy as np
 
 def saliency(prediction_logit, token_ids_tensor_one_hot, norm=True):
     # Back-propegate the gradient from the selected output-logit
-    prediction_logit.backward(retain_graph=True)
+    grad = torch.autograd.grad(prediction_logit, token_ids_tensor_one_hot)[0]
 
     # token_ids_tensor_one_hot.grad is the gradient propegated to ever embedding dimension of
     # the input tokens.
     if norm:  # norm calculates a scalar value (L2 Norm)
-        token_importance_raw = torch.norm(token_ids_tensor_one_hot.grad, dim=1)
+        token_importance_raw = torch.norm(grad, dim=1)
         # print('token_importance_raw', token_ids_tensor_one_hot.grad.shape,
         # np.count_nonzero(token_ids_tensor_one_hot.detach().numpy(), axis=1))
 
         # Normalize the values so they add up to 1
         token_importance = token_importance_raw / torch.sum(token_importance_raw)
     else:
-        token_importance = torch.sum(token_ids_tensor_one_hot.grad, dim=1)  # Only one value, all others are zero
+        token_importance = torch.sum(grad, dim=1)  # Only one value, all others are zero
 
     token_ids_tensor_one_hot.grad.data.zero_()
     return token_importance
@@ -26,7 +26,6 @@ def saliency_on_d_embeddings(prediction_logit, inputs_embeds, aggregation="L2"):
     inputs_embeds.retain_grad()
 
     # Back-propegate the gradient from the selected output-logit
-    prediction_logit.backward(retain_graph=True)
 
     # inputs_embeds.grad
     # token_ids_tensor_one_hot.grad is the gradient propegated to ever embedding dimension of
@@ -50,13 +49,9 @@ def saliency_on_d_embeddings(prediction_logit, inputs_embeds, aggregation="L2"):
 
 
 def gradient_x_inputs_attribution(prediction_logit, inputs_embeds):
-
     inputs_embeds.retain_grad()
     # back-prop gradient
-    prediction_logit.backward(retain_graph=True)
-    grad = inputs_embeds.grad
-    # This should be equivalent to
-    # grad = torch.autograd.grad(prediction_logit, inputs_embeds)[0]
+    grad = torch.autograd.grad(prediction_logit, inputs_embeds)[0]
 
     # Grad X Input
     grad_x_input = grad * inputs_embeds
