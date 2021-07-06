89eade60399a0994c3d2bad57e1863972f2340bc
Jay <alammar@gmail.com>
2021-07-06 12:01:13 +0300

    - Setting torch version to 1.7.1 (Troubleshooting failing test)

--- tests/attribution_test.py
+++ tests/attribution_test.py
@@ -7,7 +7,7 @@ import pytest
 def simpleNNModel():
     class simpleNNModel(torch.nn.Module):
         def __init__(self):
-            super(simpleNNModel, self).__init__()
+            super().__init__()
             self.w = torch.tensor([[10., 10.]])
 
         def forward(self, x):
@@ -17,11 +17,8 @@ def simpleNNModel():
 
 class TestAttribution:
     def test_grad_x_input(self, simpleNNModel):
-        input = torch.tensor([[9., 9.]], requires_grad=True)
-        print(input.device)
-        output = simpleNNModel(input)
-        print(output.device,output.shape, output[0].shape, output[0][0] )
+        input_ = torch.tensor([[9., 9.]], requires_grad=True)
+        output = simpleNNModel(input_)
         expected = torch.tensor([1.])
-        actual = gradient_x_inputs_attribution(output[0][0],input)
+        actual = gradient_x_inputs_attribution(output[0][0],input_)
         assert torch.all(torch.eq(actual, expected))
-        assert False
