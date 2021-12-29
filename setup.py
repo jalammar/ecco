18a17b903df7c85e612c8ca5292c998c873f8e3b
Jay <alammar@gmail.com>
2021-12-29 13:23:49 +0300

    bump version number

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='ecco',
-    version='0.0.15',
+    version='0.1.0',
     license='BSD-3-Clause',
     description='Visualization tools for NLP machine learning models.',
     long_description='%s\n%s' % (
